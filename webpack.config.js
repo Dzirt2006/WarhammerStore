a53ffcf2d629750ee8b44e18f044a67878e2a445
Alex <aleksei_pavlov@yahoo.com>
2020-05-31 17:41:09 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') 
+
+module.exports = {
+  watch: false,
+  mode: 'development',
+  entry: './app/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  //babel
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  //HtmlWebpackPlugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ] 
+}
